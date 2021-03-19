f4492ce0d714ea654009e81821fce06cfaf0b371
JacobSutherland <jacobsutherland224@gmail.com>
2021-03-19 01:34:53 -0500

    connected templates to JSON data, updated information, and styled home page

--- router/index.js
+++ router/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../data.json');
+const projects = data.projects;
 
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { data: projects });
 })
 
 router.get('/about', (req, res) => {
@@ -11,7 +13,18 @@ router.get('/about', (req, res) => {
 });
 
 router.get('/project', (req, res) => {
-    res.render('project');
+    res.render('project')
+    
+});
+
+router.get('/project/:id', (req, res) => {
+    const page = req.params.id;
+    res.render('project', 
+    { 
+        data: projects[page],
+        technologies: projects[page].technologies,
+        images: projects[page].image_urls
+    });
 });
 
 module.exports = router;
