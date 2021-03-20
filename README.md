fb52b8c5eb241cf3d497fe049aec3882fbbf3784
JacobSutherland <jacobsutherland224@gmail.com>
2021-03-20 18:22:31 -0500

    Error handler modified, added NPM start command, declutterd pug files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # techdegree-project-6
 Portfolio  page created using Node.js, Express.js, and Pug to serve backend JavaScript and templates that feed the frontend with JSON data for my personal projects and accomplishments.
+
+1. Updated background-color, color, fonts, skills/tools, and buttons.
+2. added error and 404 pages
+3. NPM Start
--- router/index.js
+++ router/index.js
@@ -1,41 +1,55 @@
 const express = require('express');
-const { render } = require('pug');
 const router = express.Router();
 const data = require('../data.json');
 const projects = data.projects;
 
-
+//Home page
 router.get('/', (req, res) => {
     res.render('index', { data: projects });
 })
 
+//Abou page
 router.get('/about', (req, res) => {
     res.render('about');
 });
 
-router.get('/project/:id', (req, res) => {
+//Project page
+//renders projects by checking search parameter against existing ids
+router.get('/project/:id', (req, res, next) => {
     const page = req.params.id;
-    res.render('project', 
-    { 
-        data: projects[page],
-        technologies: projects[page].technologies,
-        images: projects[page].image_urls
-    });
+    //lists all existing ids
+    const pages = projects.map( i => i.id);
+    //checks if id exists and renders page
+    if(pages.includes(page)){
+        res.render('project', 
+        { 
+            data: projects[page],
+            technologies: projects[page].technologies,
+            images: projects[page].image_urls
+        });
+    } else {
+        next();
+    }
 });
-
+//General Error Handler 
 router.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    res.render('page-not-found', { error: error });
+    const err = new Error('Uh oh, think you might be lost');
+    err.status = 404 || 500;
+    err.message = 'Not Found';
     next(err);
 });
 
+//recives Error object and throws error based on status code
 router.use((err, req, res, next) => {
-    const error = err;
-    err.status = 500;
-    err.message = 'something went wrong'
-    res.render('error', { error: error })
-    console.log(err.status, err.message);
+    if(err.status === 404) {
+        res.render('page-not-found', { err: err });
+        console.log(err.status, err.message);
+    } else {
+        err.message = 'Something Went Wrong';
+        err.status = 500;
+        res.render('error', { err: err });
+        console.log(err.status, err.message);
+    };
 });
 
 module.exports = router
