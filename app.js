df88796d84f9d41d74c34c91e56db8afd48bfdc1
JacobSutherland <jacobsutherland224@gmail.com>
2021-03-21 00:21:09 -0500

    added hosting files and config

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const routes = require('./router/index.js');
+let port = process.env.PORT || 3000
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -9,4 +10,4 @@ app.use(routes);
 
 app.set('view engine', 'pug');
 
-app.listen(3000, console.log('Server is live on port 3000'));
+app.listen(3000, console.log(`Server is live on port ${port}`));
